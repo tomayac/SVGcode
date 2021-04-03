b0b0a525c97a4b1b592c337f2fd13b428b62de8d
tomayac <steiner.thomas@gmail.com>
2021-04-03 19:13:41 +0200

    Remove dist for now

--- dist/assets/file-open.c1794569.js
+++
@@ -1,2 +0,0 @@
-// @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
-const e=async e=>{const t=await e.getFile();return t.handle=e,t};export default async(t={})=>{const i=await window.chooseFileSystemEntries({accepts:[{description:t.description||"",mimeTypes:t.mimeTypes||["*/*"],extensions:t.extensions||[""]}],multiple:t.multiple||!1});return t.multiple?Promise.all(i.map(e)):e(i)};
--- dist/assets/file-save.72f7afaf.js
+++
@@ -1,2 +0,0 @@
-// @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
-export default async(e,t={})=>{const c=document.createElement("a");c.download=t.fileName||"Untitled",c.href=URL.createObjectURL(e),c.addEventListener("click",(()=>{setTimeout((()=>URL.revokeObjectURL(c.href)),3e4)})),c.click()};
