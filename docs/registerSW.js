d0add446f3e5c6dff249339a0266fed189df69d8
tomayac <steiner.thomas@gmail.com>
2021-04-09 11:56:11 +0200

    No base URL

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/SVGcode/sw.js', { scope: '/SVGcode/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
