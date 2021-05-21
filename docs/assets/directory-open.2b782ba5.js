173ee72a666e6e1f2bf9e7a44eb1b6b2b90a4714
tomayac <steiner.thomas@gmail.com>
2021-05-21 14:36:13 +0200

    Deploy

---
+++ docs/assets/directory-open.2b782ba5.js
@@ -0,0 +1,2 @@
+// @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
+const e=async(r,t,a=r.name)=>{const i=[],n=[];for await(const o of r.values()){const c=`${a}/${o.name}`;"file"===o.kind?n.push(o.getFile().then((e=>(e.directoryHandle=r,Object.defineProperty(e,"webkitRelativePath",{configurable:!0,enumerable:!0,get:()=>c}))))):"directory"===o.kind&&t&&i.push(e(o,t,c))}return[...(await Promise.all(i)).flat(),...await Promise.all(n)]};export default async(r={})=>{r.recursive=r.recursive||!1;const t=await window.showDirectoryPicker();return e(t,r.recursive)};
--- docs/assets/directory-open.91239a8f.js
+++
@@ -1,2 +0,0 @@
-// @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
-const e=async(t,r,a=t.name)=>{const i=[],n=[];for await(const o of t.values()){const t=`${a}/${o.name}`;"file"===o.kind?n.push(o.getFile().then((e=>Object.defineProperty(e,"webkitRelativePath",{configurable:!0,enumerable:!0,get:()=>t})))):"directory"===o.kind&&r&&i.push(e(o,r,t))}return[...(await Promise.all(i)).flat(),...await Promise.all(n)]};export default async(t={})=>{t.recursive=t.recursive||!1;const r=await window.showDirectoryPicker();return e(r,t.recursive)};
