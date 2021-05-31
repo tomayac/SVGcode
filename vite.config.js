1747e48f58ad38fc8a1cc2f0f9cc3e9a3eb4a232
Thomas Steiner <tomac@google.com>
2021-05-31 23:03:30 +0200

    Switch to modules target

--- vite.config.js
+++ vite.config.js
@@ -36,6 +36,6 @@ export default {
   ],
   build: {
     outDir: 'docs',
-    target: 'esnext',
+    target: 'modules', // Switch to 'esnext' once Module Workers work in Firefox and Safari.
   },
 };
