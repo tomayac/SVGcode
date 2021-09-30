b2e228889a966a11f9d434f94cec6c4552fe4133
Thomas Steiner <tomac@google.com>
2021-09-30 15:32:33 +0200

    Manifest

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { VitePWA as vitePWA } from 'vite-plugin-pwa';
 import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
+import webmanifest from './src/manifest.json';
 
 // ignore unused exports default
 export default {
@@ -7,49 +8,7 @@ export default {
     dynamicImportVars({
       include: ['./src/i18n/*', './src/js/filehandling.js'],
     }),
-    vitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      manifest: {
-        name: 'SVGcode',
-        short_name: 'SVGcode',
-        start_url: './',
-        scope: './',
-        display: 'standalone',
-        orientation: 'portrait',
-        theme_color: '#131313',
-        background_color: '#131313',
-        icons: [
-          {
-            src: './favicon.svg',
-            type: 'image/svg+xml',
-            sizes: '340x340',
-          },
-          {
-            src: './favicon.png',
-            type: 'image/png',
-            sizes: '768x768',
-          },
-        ],
-        file_handlers: [
-          {
-            action: './',
-            accept: {
-              'image/*': [
-                '.jpg',
-                '.jpeg',
-                '.webp',
-                '.png',
-                '.avif',
-                '.gif',
-                '.svg',
-                '.bmp',
-              ],
-            },
-          },
-        ],
-      },
-    }),
+    vitePWA(webmanifest),
   ],
   build: {
     outDir: 'docs',
