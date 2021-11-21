65c3df2cfa420b92818e09f34a5cca5f3f3fdf7e
Jacob Groß <kurtextrem@gmail.com>
2021-11-21 13:19:27 +0100

    Add german translation
    fixes the symptoms of #31

---
+++ src/i18n/de.js
@@ -0,0 +1,66 @@
+const translations = {
+  red: 'Rot',
+  green: 'Grün',
+  blue: 'Blau',
+  alpha: 'Alpha',
+
+  brightness: 'Helligkeit',
+  contrast: 'Kontrast',
+  grayscale: 'Graustufen',
+  'hue-rotate': 'Hue ändern',
+  invert: 'Invertieren',
+  opacity: 'Transparenz',
+  saturate: 'Sättigung',
+  sepia: 'Sepia',
+
+  scale: 'Skala',
+  turdsize: 'Flecken unterdrücken',
+  alphamax: 'Eckschwelle',
+  minPathSegments: 'Min. Pfadlänge',
+  strokeWidth: 'Strichbreite',
+  turnpolicy: 'Turn Policy',
+  opticurve: 'Kurven optimieren',
+  opttolerance: 'Optimierungs Toleranz',
+  showAdvancedControls: 'Expertenoptionen anzeigen',
+
+  '%': '%',
+  deg: '°',
+  steps: 'Scrhtitte',
+  pixels: 'Pixel',
+  segments: 'Segmente',
+
+  reset: 'Zurücksetzen',
+  resetAll: 'Alles zurücksetzen',
+
+  dropFileHere: 'Datei hier ablegen',
+  openImage: 'Bild öffnen',
+  saveSVG: 'SVG speichern',
+  pasteImage: 'Bild einfügen',
+  copySVG: 'SVG kopieren',
+  install: 'Installieren',
+
+  posterizeInputImage: 'Eingabebild posterisieren',
+  colorSVG: 'Farbiges SVG',
+  monochromeSVG: 'Monochromes SVG',
+
+  colorChannels: 'Farbkanäle',
+  imageSize: 'Eingabegröße',
+  imagePreprocessing: 'Eingabe verarbeiten',
+  svgOptions: 'SVG Optionen',
+
+  considerDPR: 'Device Pixel Ratio beachten',
+
+  tweak: 'Anpassen',
+
+  optimizingSVG: 'SVG optmieren',
+  copiedSVG: 'SVG kopiert',
+  savedSVG: 'SVG gespeichert',
+
+  readyToWorkOffline: 'Bereit für Offline-Nutzung.',
+  svgSize: 'SVG Größe',
+  bytes: 'Bytes',
+  zoom: 'Zoom',
+};
+
+// ignore unused exports default
+export default translations;
