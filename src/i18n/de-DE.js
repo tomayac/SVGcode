3d2409ece8dcf1a914ec10d2e0d51e171ffcc10f
Thomas Steiner <tomac@google.com>
2021-11-22 15:58:42 +0100

    Add Greek and German translations

---
+++ src/i18n/de-DE.js
@@ -0,0 +1,66 @@
+const translations = {
+  red: 'Rot',
+  green: 'Grün',
+  blue: 'Blau',
+  alpha: 'Alpha',
+
+  brightness: 'Helligkeit',
+  contrast: 'Kontrast',
+  grayscale: 'Graustufen',
+  'hue-rotate': 'Farbtonrotation',
+  invert: 'Invertieren',
+  opacity: 'Transparenz',
+  saturate: 'Sättigung',
+  sepia: 'Sepia',
+
+  scale: 'Skalierung',
+  turdsize: 'Flecken unterdrücken',
+  alphamax: 'Eckenschwellwert',
+  minPathSegments: 'Min. Pfadlänge',
+  strokeWidth: 'Strichbreite',
+  turnpolicy: 'Wendestrategie',
+  opticurve: 'Kurven optimieren',
+  opttolerance: 'Optimierungstoleranz',
+  showAdvancedControls: 'Expertenoptionen anzeigen',
+
+  '%': '%',
+  deg: '°',
+  steps: 'Schritte',
+  pixels: 'Pixel',
+  segments: 'Segmente',
+
+  reset: 'Zurücksetzen',
+  resetAll: 'Alles zurücksetzen',
+
+  dropFileHere: 'Datei hier ablegen',
+  openImage: 'Bild öffnen',
+  saveSVG: 'SVG speichern',
+  pasteImage: 'Bild einfügen',
+  copySVG: 'SVG kopieren',
+  install: 'Installieren',
+
+  posterizeInputImage: 'Eingabebild posterisieren',
+  colorSVG: 'Farbiges SVG',
+  monochromeSVG: 'Monochromes SVG',
+
+  colorChannels: 'Farbkanäle',
+  imageSize: 'Bildgröße',
+  imagePreprocessing: 'Bildvorverarbeitung',
+  svgOptions: 'SVG-Optionen',
+
+  considerDPR: 'Pixelverhältnis des Geräts beachten',
+
+  tweak: 'Anpassen',
+
+  optimizingSVG: 'Optmiere SVG',
+  copiedSVG: 'SVG kopiert',
+  savedSVG: 'SVG gespeichert',
+
+  readyToWorkOffline: 'Bereit für Offline-Nutzung.',
+  svgSize: 'SVG-Größe',
+  bytes: 'Byte',
+  zoom: 'Zoom',
+};
+
+// ignore unused exports default
+export default translations;
