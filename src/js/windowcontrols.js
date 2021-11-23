a7dcd0eefd9d813e17de4417404685fe62f19f61
Thomas Steiner <tomac@google.com>
2021-11-23 09:41:15 +0100

    Run geometryChange once

--- src/js/windowcontrols.js
+++ src/js/windowcontrols.js
@@ -3,15 +3,21 @@ import { main, menu } from './domrefs.js';
 
 const WINDOW_CONTROLS_OVERLAY = 'window-controls-overlay';
 
+const onGeometryChange = () => {
+  if (navigator.windowControlsOverlay.visible) {
+    menu.classList.add(WINDOW_CONTROLS_OVERLAY);
+    main.classList.add(WINDOW_CONTROLS_OVERLAY);
+  } else {
+    menu.classList.remove(WINDOW_CONTROLS_OVERLAY);
+    main.classList.remove(WINDOW_CONTROLS_OVERLAY);
+  }
+};
+
 navigator.windowControlsOverlay.addEventListener(
   'geometrychange',
   debounce(async () => {
-    if (navigator.windowControlsOverlay.visible) {
-      menu.classList.add(WINDOW_CONTROLS_OVERLAY);
-      main.classList.add(WINDOW_CONTROLS_OVERLAY);
-    } else {
-      menu.classList.remove(WINDOW_CONTROLS_OVERLAY);
-      main.classList.remove(WINDOW_CONTROLS_OVERLAY);
-    }
+    onGeometryChange();
   }, 250),
 );
+
+onGeometryChange();
