d48f500b33d0a4972259fdd19ef5fbcd4dcc55b4
George Raptis <georapbox@gmail.com>
2021-11-25 22:15:52 +0200

    Update src/i18n/de-DE.js
    Co-authored-by: Thomas Steiner <tomac@google.com>

--- src/i18n/de-DE.js
+++ src/i18n/de-DE.js
@@ -70,7 +70,7 @@ const translations = {
   considerDPR: 'Pixelverhältnis des Geräts beachten',
 
   tweak: 'Anpassen',
-  closeOptions: 'Schließung',
+  closeOptions: 'Schließen',
 
   optimizingSVG: 'Optmiere SVG',
   copiedSVG: 'SVG kopiert',
