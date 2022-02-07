d132d770eca6fa32236fa4cb945b78b11542f063
Ron Derksen <me@ronderksen.nl>
2022-02-07 15:52:08 +0100

    Add Dutch translation

---
+++ src/i18n/nl-NL.js
@@ -0,0 +1,96 @@
+/**
+ * SVGcode—Convert raster images to SVG vector graphics
+ * Copyright (C) 2021 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+const translations = {
+  red: 'Rood',
+  green: 'Groen',
+  blue: 'Blauw',
+  alpha: 'Alfa',
+
+  brightness: 'Helderheid',
+  contrast: 'Contrast',
+  grayscale: 'Grijswaarden',
+  'hue-rotate': 'Hue Rotate',
+  invert: 'Omdraaien',
+  opacity: 'Transparantie',
+  saturate: 'Verzadiging',
+  sepia: 'Sepia',
+
+  scale: 'Schaal',
+  rotation: 'Rotatie',
+  turdsize: 'Onderdruk spikkels',
+  alphamax: 'Corner Threshold',
+  minPathSegments: 'Min. Padlengte',
+  strokeWidth: 'Lijndikte',
+  turnpolicy: 'Turn Policy',
+  opticurve: 'Optimaliseer Curves',
+  opttolerance: 'Optimalisatie Tolerantie',
+  showAdvancedControls: 'Toon Expert Opties',
+
+  '%': '%',
+  deg: '°',
+  steps: 'Stappen',
+  pixels: 'Pixels',
+  segments: 'Segmenten',
+
+  reset: 'Reset',
+  resetAll: 'Reset Alles',
+
+  dropFileHere: 'Drop bestand hier',
+  openImage: 'Open beeldbestand',
+  saveSVG: 'SVG opslaan',
+  pasteImage: 'Beeldbestand plakken',
+  copySVG: 'SVG kopiëren',
+  install: 'Installeren',
+
+  posterizeInputImage: 'Invoerbeeld Posteriseren',
+  colorSVG: 'Kleuren SVG',
+  monochromeSVG: 'Monochrome SVG',
+
+  colorChannels: 'Kleurkanalen',
+  imageSizeAndRotation: 'Invoergrootte en Rotatie',
+  imagePreprocessing: 'Invoer Voorbewerking',
+  svgOptions: 'SVG Opties',
+
+  considerDPR: 'Consider Device Pixel Ratio',
+
+  tweak: 'Tweak',
+  closeOptions: 'Sluiten',
+
+  optimizingSVG: 'SVG aan het optimaliseren',
+  copiedSVG: 'SVG gekopiëerd',
+  savedSVG: 'SVG opgeslagen',
+
+  readyToWorkOffline: 'Klaar om offline te werken.',
+  svgSize: 'SVG grootte',
+  bytes: 'Bytes',
+  zoom: 'Inzoomen',
+
+  license: 'Licentie',
+  about: 'Over',
+
+  enUS: 'Engels (Verenigde Staten)',
+  deDE: 'Duits (Duitsland)',
+  elGR: 'Grieks (Griekenland)',
+  koKR: 'Koreaans (Korea)',
+  zhCN: 'Chinees (China)',
+};
+
+// ignore unused exports default
+export default translations;
