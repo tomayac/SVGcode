35c9360577a04a242bcb4c1cd6c5832df970f722
Thomas Steiner <tomac@google.com>
2022-02-15 16:01:14 +0100

    Add Indonesian locale

---
+++ docs/assets/filehandling.03d74b7a.js
@@ -0,0 +1,1 @@
+import{i as o,F as r}from"./index.2499c267.js";import{s as n}from"./vendor.78f84693.js";window.launchQueue.setConsumer(async e=>{if(!!e.files.length)for(const t of e.files){const i=await t.getFile();if(i.type.startsWith("image/")){const s=URL.createObjectURL(i);o.addEventListener("load",()=>{URL.revokeObjectURL(s)},{once:!0}),o.src=s,await n(r,t);return}}});
--- src/i18n/id-ID.js
+++ src/i18n/id-ID.js
@@ -17,79 +17,78 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
- import languages from './languages.js';
+import languages from './languages.js';
 
- const translations = {
-   red: 'Merah',
-   green: 'Hijau',
-   blue: 'Biru',
-   alpha: 'Alfa',
- 
-   brightness: 'Cahaya',
-   contrast: 'Kontras',
-   grayscale: 'Monokromatik',
-   'hue-rotate': 'Perputaran Corak',
-   invert: 'Inversi',
-   opacity: 'Transparansi',
-   saturate: 'Saturasi',
-   sepia: 'Sepia',
- 
-   scale: 'Skala',
-   rotation: 'Rotasi',
-   turdsize: 'Hapus Bintik',
-   alphamax: 'Batas Pojok',
-   minPathSegments: 'Panjang Minimal Garis',
-   strokeWidth: 'Lebar Goresan',
-   turnpolicy: 'Balik Haluan',
-   opticurve: 'Optimasi Lengkungan',
-   opttolerance: 'Optimasi Toleransi',
-   showAdvancedControls: 'Munculkan Opsi Tambahan',
- 
-   '%': '%',
-   deg: '°',
-   steps: 'Langkah',
-   pixels: 'Piksel',
-   segments: 'Segmen',
- 
-   reset: 'Atur Ulang',
-   resetAll: 'Atur Ulang Semua',
- 
-   dropFileHere: 'Taruh File Di sini',
-   openImage: 'Buka Gambar',
-   saveSVG: 'Simpan SVG',
-   pasteImage: 'Tempel Gambar',
-   copySVG: 'Salin SVG',
-   install: 'Pasang',
- 
-   posterizeInputImage: 'Posterisasi Gambar',
-   colorSVG: 'SVG Berwarna',
-   monochromeSVG: 'SVG Hitam Putih',
- 
-   colorChannels: 'Kanal Warna',
-   imageSizeAndRotation: 'Ukuran dan Rotasi',
-   imagePreprocessing: 'Pra Pemrosesan',
-   svgOptions: 'Pengaturan SVG',
- 
-   considerDPR: 'Pertimbangkan Proporsi Piksel Perangkat',
- 
-   tweak: 'Modifikasi',
-   closeOptions: 'Tutup',
- 
-   optimizingSVG: 'Optimasi SVG',
-   copiedSVG: 'SVG Tersalin',
-   savedSVG: 'SVG Tersimpan',
- 
-   readyToWorkOffline: 'Siap Bekerja Offline',
-   svgSize: 'Ukuran SVG',
-   bytes: 'Bytes',
-   zoom: 'Perbesar',
- 
-   license: 'Lisensi',
-   about: 'Tentang',
- 
-   ...languages,
- };
- 
- // ignore unused exports default
- export default translations;
- 
+const translations = {
+  red: 'Merah',
+  green: 'Hijau',
+  blue: 'Biru',
+  alpha: 'Alfa',
+
+  brightness: 'Cahaya',
+  contrast: 'Kontras',
+  grayscale: 'Monokromatik',
+  'hue-rotate': 'Perputaran Corak',
+  invert: 'Inversi',
+  opacity: 'Transparansi',
+  saturate: 'Saturasi',
+  sepia: 'Sepia',
+
+  scale: 'Skala',
+  rotation: 'Rotasi',
+  turdsize: 'Hapus Bintik',
+  alphamax: 'Batas Pojok',
+  minPathSegments: 'Panjang Minimal Garis',
+  strokeWidth: 'Lebar Goresan',
+  turnpolicy: 'Balik Haluan',
+  opticurve: 'Optimasi Lengkungan',
+  opttolerance: 'Optimasi Toleransi',
+  showAdvancedControls: 'Munculkan Opsi Tambahan',
+
+  '%': '%',
+  deg: '°',
+  steps: 'Langkah',
+  pixels: 'Piksel',
+  segments: 'Segmen',
+
+  reset: 'Atur Ulang',
+  resetAll: 'Atur Ulang Semua',
+
+  dropFileHere: 'Taruh File Di sini',
+  openImage: 'Buka Gambar',
+  saveSVG: 'Simpan SVG',
+  pasteImage: 'Tempel Gambar',
+  copySVG: 'Salin SVG',
+  install: 'Pasang',
+
+  posterizeInputImage: 'Posterisasi Gambar',
+  colorSVG: 'SVG Berwarna',
+  monochromeSVG: 'SVG Hitam Putih',
+
+  colorChannels: 'Kanal Warna',
+  imageSizeAndRotation: 'Ukuran dan Rotasi',
+  imagePreprocessing: 'Pra Pemrosesan',
+  svgOptions: 'Pengaturan SVG',
+
+  considerDPR: 'Pertimbangkan Proporsi Piksel Perangkat',
+
+  tweak: 'Modifikasi',
+  closeOptions: 'Tutup',
+
+  optimizingSVG: 'Optimasi SVG',
+  copiedSVG: 'SVG Tersalin',
+  savedSVG: 'SVG Tersimpan',
+
+  readyToWorkOffline: 'Siap Bekerja Offline',
+  svgSize: 'Ukuran SVG',
+  bytes: 'Bytes',
+  zoom: 'Perbesar',
+
+  license: 'Lisensi',
+  about: 'Tentang',
+
+  ...languages,
+};
+
+// ignore unused exports default
+export default translations;
