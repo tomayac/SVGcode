9c65b925706cf61aca601c573b67e2d8f719e893
Thomas Steiner <tomac@google.com>
2022-04-04 18:08:56 +0200

    Uncommment variable

--- src/js/main.js
+++ src/js/main.js
@@ -20,7 +20,7 @@
 import { initUI, showToast } from './ui.js';
 import { registerSW } from 'virtual:pwa-register';
 import { i18n } from './i18n.js';
-import { installButton, shareSVGButton /* , inputImage*/ } from './domrefs.js';
+import { installButton, shareSVGButton, inputImage } from './domrefs.js';
 
 if ('launchQueue' in window) {
   import('./filehandling.js');
