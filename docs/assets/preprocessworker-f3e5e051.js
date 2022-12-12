5586a404f9ac6d0741fd881e86efecbc0d204710
Thomas Steiner <tomac@google.com>
2022-12-12 11:39:45 +0100

    Move to Vite 4

---
+++ docs/assets/preprocessworker-f3e5e051.js
@@ -0,0 +1,1 @@
+function r(){return new Worker("/assets/preprocessworker-0f706d1d.js")}export{r as default};
--- docs/assets/preprocessworker.c15347fd.js
+++
@@ -1,1 +0,0 @@
-function r(){return new Worker("/assets/preprocessworker.c16fd16a.js")}export{r as default};
