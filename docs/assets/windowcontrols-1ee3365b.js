afe2ed7bce2b4ad8c79f3733914b5d6615dba179
Thomas Steiner <tomac@google.com>
2023-09-14 11:03:07 +0200

    New version

--- docs/assets/windowcontrols-1ee3365b.js
+++
@@ -1,1 +0,0 @@
-import{d as a,m as e,a as s}from"./index-3652db51.js";const o="window-controls-overlay",n=()=>{e.classList.toggle(o,navigator.windowControlsOverlay.visible),s.classList.toggle(o,navigator.windowControlsOverlay.visible)};navigator.windowControlsOverlay.addEventListener("geometrychange",a(async()=>{n()},250));n();
