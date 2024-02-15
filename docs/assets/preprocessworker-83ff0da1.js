763f879213cff1249cdab14c11cb384a2f6d7ad9
Thomas Steiner <tomac@google.com>
2024-02-15 19:47:05 +0100

    Better focus ring for dark mode toggle

--- docs/assets/preprocessworker-83ff0da1.js
+++
@@ -1,1 +0,0 @@
-function r(){return new Worker("/assets/preprocessworker-b268b8a0.js")}export{r as default};
---
+++ docs/assets/preprocessworker-CehXHJLM.js
@@ -0,0 +1,1 @@
+function r(e){return new Worker("/assets/preprocessworker-BsUKOZia.js",{name:e?.name})}export{r as default};
